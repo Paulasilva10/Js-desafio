ee93385007cbcb001e0610a575eea86ca356e79d
Paulasilva10 <paulasilva0ad@gmail.com>
2024-02-02 07:56:56 -0300

    async

--- projeto JS/javascript.js
+++ projeto JS/javascript.js
@@ -1,15 +1,25 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-Select")
 
-function convertValues() {
+const convertValues = async() => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") // outro moedas
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
-    const japaneseToday = 5
-    const australiandollar = 4
+
+    const data  = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,AUD-BRL,JPY-BRL").then(response => response.json())
+
+    const dolarToday =data.USDBRL.high
+    const euroToday =data.EURBRL.high
+    const japaneseToday =data.JPYBRL.high
+    const australiandollar =data.AUDBRL.high
+ 
+    console.log(data)
+
+    // const dolarToday = 5.2
+    // const euroToday = 6.2
+    // const japaneseToday = 5
+    // const australiandollar = 4
 
 
     if (currencySelect.value == "dolar") { // se o select estiver selecionado o valor dolar, entre aqui
